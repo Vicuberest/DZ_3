357bc796a575f19bce05c2d749472b6fc09216f0
Victoria <vica-genrietka@yandex.ru>
2022-08-14 02:08:40 +0300

    Zadacha21

---
+++ Zadacha21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите координату по оси оХ точки A: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси оY точки A: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси оZ точки A: ");
+int zA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси оХ точки B: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси оY точки B: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси оZ точки B: ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double Distance = Math.Sqrt((Math.Pow(xA-xB,2))+(Math.Pow(yA-yB,2))+(Math.Pow(zA-zB,2)));
+Console.WriteLine ($"Расстояние: {Math.Round(Distance,2)}");
