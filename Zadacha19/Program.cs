dbe500256a9c200c98cfd7e90e4009445ecec56b
Victoria <vica-genrietka@yandex.ru>
2022-08-14 02:03:12 +0300

    Zadacha19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,7 @@
+﻿string number = Console.ReadLine();
+
+if ( number[0]==number[4] && number[1]==number[3])
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else Console.WriteLine("Число не является палиндромом");
